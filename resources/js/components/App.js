5743b8bfbc8a262f98068d570ffe3c9a6cd4fa03
unknown <rashalismath@gmail.com>
2020-11-24 16:19:01 +0530

    construction message added

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -32,6 +32,12 @@ class App extends Component {
         return (
             <ThemeProvider theme={theme}>
                 <div>
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <strong>Hey yo! </strong> This site is still under construction. :)
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
                     <header>
                         <Nav></Nav>
 
